268e3e625e9178dd04ba1392221708cb701b933c
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-11-22 15:22:35 +0100

    Creazione della repo e creazione variabie

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string titoloLibro = "";
+string autoreLibro = "";
+string isbnLibro = "";
+int numeroPagineLibro = 0;
+float pesoLibro = 0;
+float larghezzaLibro = 0;
+float altezzaLibro = 0;
+float profonditaLibro = 0;
+float valutazioneMediaLibro = 0;
+int numeroRecenzioniLibro = 0;
+bool versioneKindle = false;
+bool copertinaFlessibile = false;
+
+
+
+
+
+
